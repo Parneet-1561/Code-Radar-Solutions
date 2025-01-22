e9f3500e1b781a69b25bc2aa6581783b7a5f7c53
Parneet-1561 <parneet1561.becse24@chitkara.edu.in>
2025-01-22 20:34:33 +0530

    Solution to Input and Print a Character

---
+++ Input and Print a Character@@6788d5012db25efec946f93b/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int A;
+    printf("Enter the number: ");
+    scanf("%d",&A);
+    printf("%d", A);
+    return 0;
+}
