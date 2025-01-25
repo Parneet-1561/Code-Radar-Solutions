8c4b7aafb53bb635c4ae5142f775f878fb129056
Parneet-1561 <parneet1561.becse24@chitkara.edu.in>
2025-01-25 19:52:04 +0530

    Solution to Check Positive or Negative

--- Check Positive or Negative@@6788dd63490637cfc44e1b8f/code.c
+++ Check Positive or Negative@@6788dd63490637cfc44e1b8f/code.c
@@ -5,8 +5,11 @@ int main() {
     scanf("%d", &x);
     if (x >= 0) {
         printf("Positive");
-    } else {
+    } 
+    if (x <= 0) {
         printf("Negative");
+    } else {
+        printf("zero");
     }
     return 0;
 }
