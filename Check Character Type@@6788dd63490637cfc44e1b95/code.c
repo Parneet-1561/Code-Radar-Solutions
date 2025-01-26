72942718bdc1b1ac2b17319887032632ee172d8a
Parneet-1561 <parneet1561.becse24@chitkara.edu.in>
2025-01-26 16:00:02 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,21 +6,11 @@ int main() {
     if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
         printf("Vowel");
     } else {
-        printf("Consonant");
+        printf("Consonant\n");
     } if (ch < 10 && ch > 0) {
-        printf("Digit");
+        printf("Digit\n");
     } else {
         printf("Special Character");
     }
-
-
-
-
-
-
-
-
-
-
     return 0;
 }
