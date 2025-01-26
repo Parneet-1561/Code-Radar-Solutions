55f4ff1e338e7764460ac55faffe1c7e427e9f36
Parneet-1561 <parneet1561.becse24@chitkara.edu.in>
2025-01-26 15:24:56 +0530

    Solution to Check Alphabet Case

---
+++ Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    scanf("%d", &x);
+    if (isupper(ch)) {
+        printf(Uppercase);
+    } else {
+        printf(Lowercase);
+    }
+    return 0;
+}
