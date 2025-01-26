821df98e89d9bcd3b5d9e549a5526eae29264b3f
Parneet-1561 <parneet1561.becse24@chitkara.edu.in>
2025-01-26 16:10:22 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    scanf("%d", &x);
+    if (x >= 90) {
+        printf("A");
+    }
+    if (x >= 80 && x < 90) {
+        printf("B")
+    }
+    if (x >= 70 && x < 80) {
+        printf("C")
+    } 
+    if (x >= 60 && x < 70) {
+        printf("D")
+    } 
+    if (x < 60) {
+        printf("F")
+    }
+    return 0;
+}
