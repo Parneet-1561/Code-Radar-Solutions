de5030d385364e43aca1626b28601a404febcbb5
Parneet-1561 <parneet1561.becse24@chitkara.edu.in>
2025-01-27 10:19:52 +0530

    Solution to Determine Eligibility for Voting

---
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    age x;
+    scanf("%d, &age");
+    if (age >= 18) {
+        printf("Eligible");
+    }else {
+        printf("Not ELigible");
+    }
+    return 0;
+}
