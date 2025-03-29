a1060118885f3206a60c0af6687291e14cca10be
Parneet-1561 <parneet1561.becse24@chitkara.edu.in>
2025-03-30 00:57:07 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -6,8 +6,8 @@ int main() {
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    int isPalindrome = 0;
-    for(int i = 0, j = n -1; i < j; i++, j--) {
+    int isPalindrome = 1;
+    for(int i = 0, j = n - 1; i < j; i++, j--) {
         if(arr[i] != arr[j]) {
             isPalindrome = 0;
             break;
