2104dce2379c140d3a878087a76f97dcb178e441
Parneet-1561 <parneet1561.becse24@chitkara.edu.in>
2025-03-29 19:38:45 +0530

    Solution to Prime Number Checker

---
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -0,0 +1,14 @@
+int isPrime(int num) {
+    if(num < 2) {
+        return 0;
+    } else {
+        for(int i = 2; i * i <= num; i++) {
+            if(i % num == 0) {
+                printf(0);
+                break;
+            }
+        }
+        
+    }
+}
+
